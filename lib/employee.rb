d9d368dd7311769cb127f969bd486c4bf62652ca
Stoneface-Vimes <cliffweir92@gmail.com>
2020-03-02 17:11:50 -0500

    added validation to employee and store

--- lib/employee.rb
+++ lib/employee.rb
@@ -2,4 +2,12 @@
 
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: {
+    only_interger: true,
+    greater_than: 39,
+    less_than: 201
+  }
+  validates :store_id, presence: true
 end
